3fa00a8b9f0b50ffe87c9ae3f1b8e0a4f97a4f6f
Massimo Bono <massimobono1@gmail.com>
2020-11-17 15:53:31 +0100

    add commands and model

--- pycharm/pmake/PMakeModel.py
+++ pycharm/pmake/PMakeModel.py
@@ -11,13 +11,14 @@ from pmake.commons_types import path
 from pmake.constants import STANDARD_MODULES, STANDARD_VARIABLES
 
 
-class AttrDict():
+class AttrDict(object):
 
     def __init__(self, d):
         self.__d = d
 
     def __getattr__(self, item):
         return self.__d[item]
+
 
 class PMakeModel(abc.ABC):
 
@@ -52,6 +53,12 @@ class PMakeModel(abc.ABC):
         if "variables" in result:
             raise KeyError(f"duplicate key \"variables\". It is already mapped to the value {result['variables']}")
         result["variables"] = AttrDict({k: v for k, v in self.variable})
+        if "model" in result:
+            raise KeyError(f"duplicate key \"model\". It is already mapped to the value {result['model']}")
+        result["model"] = self
+        if "commands" in result:
+            raise KeyError(f"duplicate key \"commands\". It is already mapped to the value {result['commands']}")
+        result["commands"] = self.session_script
 
         return result
 
